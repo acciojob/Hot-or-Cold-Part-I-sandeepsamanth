ae7d94388c3bb69fa92e155f4b30398b9691ffd2
sandeepsamanth <98205088+sandeepsamanth@users.noreply.github.com>
2023-02-12 23:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+ const btn = document.querySelector("#btn");
+  const guess = document.querySelector("#guess");
+  const result = document.querySelector("#respond");
+
+  let randomNum = Math.floor(Math.random() * 101); // generates random number between 0 and 100
+
+  btn.addEventListener("click", function() {
+    let userGuess = parseInt(guess.value);
+
+    if (Math.abs(userGuess - randomNum) <= 10) {
+      result.textContent = "Hot";
+    } else {
+      result.textContent = "Cold";
+    }
+  });
